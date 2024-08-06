c1da648af3041a4d13acdd198640a9342aedc3cc
WeihangLiu2024 <liuwh2023@shanghaitech.edu.cn>
2024-08-06 15:44:42 +0800

    release v0.2
    add comments

--- experiments/ngp/train_ngp.sh
+++ experiments/ngp/train_ngp.sh
@@ -1,4 +1,4 @@
-#set -x
+set -x
 
 downscale=1
 while [[ "$#" -gt 0 ]]; do
@@ -8,6 +8,7 @@ while [[ "$#" -gt 0 ]]; do
         -f|--format) format="$2"; shift ;;
         -b|--bound) aabb="$2"; shift;;
         --downscale) downscale="$2"; shift;;
+        --log2_hashmap_size) log2_hashmap_size="$2"; shift;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -15,4 +16,4 @@ done
 
 time=$(date "+%Y-%m-%d_%H:%M:%S")
 
-python ../../main.py ../../data/${data} --workspace ../../workspace/${logdir}/${data}/${time} -O --data_format ${format} --bound ${aabb} --downscale ${downscale} --update_hash 0
+python ../../main.py ../../data/${data} --workspace ../../workspace/${logdir}/${data}/${time} -O --data_format ${format} --bound ${aabb} --downscale ${downscale} --update_hash 0 --log2_hashmap_size ${log2_hashmap_size}
--- experiments/ngp/train_synthetic_ngp.sh
+++ experiments/ngp/train_synthetic_ngp.sh
@@ -10,19 +10,20 @@
 exp_name='NGP'
 data_format='nerf'
 downscale=1
+log2_hashmap_size=19
 
-./train_ngp.sh -d nerf_synthetic/chair -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/chair -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/drums -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/drums -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/ficus -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/ficus -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/hotdog -l ${exp_name} -f ${data_format} -b 1.3 --downscale ${downscale} & sleep 500s
+./train_ngp.sh -d nerf_synthetic/hotdog -l ${exp_name} -f ${data_format} -b 1.3 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 500s
 
-./train_ngp.sh -d nerf_synthetic/lego -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/lego -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/materials -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/materials -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/mic -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} & sleep 10s
+./train_ngp.sh -d nerf_synthetic/mic -l ${exp_name} -f ${data_format} -b 1.0 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} & sleep 10s
 
-./train_ngp.sh -d nerf_synthetic/ship -l ${exp_name} -f ${data_format} -b 1.3 --downscale ${downscale}
+./train_ngp.sh -d nerf_synthetic/ship -l ${exp_name} -f ${data_format} -b 1.3 --downscale ${downscale} --log2_hashmap_size ${log2_hashmap_size} &
